7a829109f98d5932ea1169f4718eaf6088c38fb2
charleskahuho <ckcharles004@gmail.com>
2023-03-16 07:21:48 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,17 @@
 /**
- * filename:printf.c
+ * filename: printf.c
  */
 
 #include <stdio.h>
 
 /**
- * main-entry point
+ * main - entry point
  *
- * return- value 0 (stated)
+ * return:  value 0 (stated)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+	
 	return (0);
 }
