fcaa4d79ce60b2dd997982406c27071829b38043
charleskahuho <ckcharles004@gmail.com>
2023-03-16 07:02:23 -0700

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* puts -print declared char string to standard output
+*
+* Return: value  0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("size of char: %d byte(s)\n", sizeof(char));
+	printf("size of int: %d byte(s)\n", sizeof(int));
+	printf("size of float: %d byte(s)\n", sizeof(float));
+	printf("size of long int: %d byte(s)\n", sizeof(long int));
+	printf("size of long long int: %d byte(s)\n", sizeof(long long int));
+	return(0);
+}
+
