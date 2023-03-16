42192de4aee29a0c5d553a5fb1142f0aa9724df2
charleskahuho <ckcharles004@gmail.com>
2023-03-16 07:31:59 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
-
-int main()
+/**
+* main - Enret point
+*
+* sizeof - determines variable size by data type prints to stdio
+*
+* Return: 0
+*/
+int main(void)
 {
-	printf("size of char: %d byte(s)\n", sizeof(char));
-	printf("size of int: %d byte(s)\n", sizeof(int));
-	printf("size of float: %d byte(s)\n", sizeof(float));
-	printf("size of long int: %d byte(s)\n", sizeof(long int));
-	printf("size of long long int: %d byte(s)\n", sizeof(long long int));
-	return(0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
 
