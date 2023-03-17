01786b4af9f048a1ed8dd5b23f077449ae753fec
charleskahuho <ckcharles004@gmail.com>
2023-03-17 09:27:28 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main - Entry point
+* printf to print whole numbers
+* Return: Always 0 
+*/
+int main(void)
+{
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar(10);
+	return (0);
+}
