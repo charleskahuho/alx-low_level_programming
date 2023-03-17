bac2c5f529aefa2f8f7756616d536d50ce5ea930
charleskahuho <ckcharles004@gmail.com>
2023-03-17 09:56:56 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+* main - Entry point
+* putchar to print combinations of sing dig numbers
+* Return: Always 0
+*/
+int main(void)
+{
+	int l = '0';
+
+	while (l <= '9')
+	{
+		putchar(i);
+		if (l < '9')
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		l++;
+	}
+	putchar(10);
+	return (0);
+}
