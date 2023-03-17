fc1a54f3298b34e0e4d7948efd98b0a6e5b22231
charleskahuho <ckcharles004@gmail.com>
2023-03-17 08:11:56 -0700

    alpabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+* main - Entry point
+* putchar with for loop-writing alphabets
+* Return: Always 
+*/
+int main(void)
+{
+	char ch = 'a';
+	char c = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(10);
+	return (0);
+}
