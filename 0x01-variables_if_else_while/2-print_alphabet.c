cfe793e5d4812b77aa7dcdec7262fbbf15dcd2e9
charleskahuho <ckcharles004@gmail.com>
2023-03-17 08:05:23 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Etry point
+ * putchar - input string
+ * return: always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for(ch='a';ch<='z';ch++)
+	{
+		putchar(ch)
+	}
+	putchar(10);
+	return (0);
+}
