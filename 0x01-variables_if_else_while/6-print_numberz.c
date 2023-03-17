fc7c8b649bf44d384b9b7b070207c237b36e49e7
charleskahuho <ckcharles004@gmail.com>
2023-03-17 09:37:17 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main -Entry point
+* putchar to print single digit numberz
+* Return: Always 0 
+*/
+int main(void)
+{
+	int k = '0';
+
+	while (k <= '9')
+	{
+		putchar(k);
+		k++;
+	}
+	putchar(10);
+	return (0);
+}
