2510e0a966f7542c914c970c99f6f8e87a13c456
charleskahuho <ckcharles004@gmail.com>
2023-03-23 07:12:27 -0700

    done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ *_isupper - function to check for uppercse character
+ *
+ * @c: parameter
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	if (_isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
