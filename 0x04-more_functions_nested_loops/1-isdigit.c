49ebb27d14e3355e4673a3efdcc2eba54fb3baf9
charleskahuho <ckcharles004@gmail.com>
2023-03-23 07:33:15 -0700

    done

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ *_isdigit - function to check for digits
+ *
+ * @c: parameter
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (_isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define MAIN "main.h"
 
 int _isupper(int c);
+int _isdigit(int c);
 
 #endif
