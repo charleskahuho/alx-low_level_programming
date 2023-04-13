9dd499bc004318ab2eee4676b8029942eb38a55b
charleskahuho <ckcharles004@gmail.com>
2023-04-13 10:25:52 -0700

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * @argc:  # no of arguements
+ * @argv: array
+ * main - function
+ * Return: 0
+ */
+int main(__attribute__ ((unused))int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
