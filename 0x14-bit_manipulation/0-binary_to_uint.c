e0fe39c264ea1e8b95c2a2b1b71b33084434eedd
charleskahuho <ckcharles004@gmail.com>
2023-05-05 01:15:22 -0700

    repository created

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include<stddef.h>
+#include"main.h"
+/**
+ * binary_to_iunt - function
+ * @b: binary to convert
+ * Return: defined
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i;
+
+	int k;
+
+	if (b == NULL)
+		return (0);
+	for (k = 0; b[k]; k++)
+	{
+		if (b[k] > '1' || b[k] < '0')
+		{
+			return (0);
+			i = 2 * i + (b[k] - '0');
+		}
+	}
+	return (i);
+}
+
+
