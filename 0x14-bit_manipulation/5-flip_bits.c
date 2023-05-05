930d46a5c99b4734b9fe4ef827c33bd0a44a018f
charleskahuho <ckcharles004@gmail.com>
2023-05-05 02:08:40 -0700

    done

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,13 +9,11 @@
 {
 	int i;
 	int count = 0;
-	unsigned long int mar;
-	unsigned long int wes;
 
 	for (i = 63; i >= 0; i--)
 	{
-		mar = wes>>i;
-		if (mar & 1)
+		n = m>>i;
+		if (n & 1)
 			count++;
 	}
 	return (count);
