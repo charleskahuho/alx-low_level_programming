6cc3bf3808aac21fcd491ab2be17a2cfeadf8c50
charleskahuho <ckcharles004@gmail.com>
2023-05-09 03:29:25 -0700

    done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include"main.h"
+#include<string.h>
+#include<stdlib.h>
+/**
+ * create file - function
+ * @filename: filename
+ * @text_content: content
+ *
+ * Return: defined
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int new_file, len, wr_stat;
+
+	if (filename == NULL)
+		return (-1);
+	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (new_file == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(new_file);
+		return (1);
+	}
+	for (len = 0; text_content[len]; len++)
+		;
+	wr_stat = write(new_file, text_content, len);
+	if (close(new_file) == -1)
+		return (-1);
+	return (wr_stat == -1 ? -1 : 1);
+}
