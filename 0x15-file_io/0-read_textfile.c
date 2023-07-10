87c2b35f811e32d296b4883ab89e26cbb42d2d6b
charleskahuho <ckcharles004@gmail.com>
2023-07-10 07:25:54 -0700

    file created

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include"main.h"
+#include<stdlib.h>
+#include<fcntl.h>
+/**
+ * read_textfile - function
+ * @filename: files name
+ * @letters: mode
+ *
+ * Return: defined
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	int length, wrotechars;
+	char *buf;
+
+	if (filename == NULL || letters == 0)
+		return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (buf == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	length = read(file, buf, letters);
+	if (length == -1)
+	{
+		free(buf);
+		close(file);
+		return (0);
+	}
+
+	wrotechars = write(STDOUT_FILENO, buf, length);
+
+	free(buf);
+	close(file);
+	if (wrotechars != length)
+		return (0);
+	return (length);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+file handloing in c
